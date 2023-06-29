1c37c1aa9afcaa0f9b4f2c5a5ccec4f632e4063d
YuliyaYurevich <yurevich@mail.com>
2023-06-29 13:23:50 -0500

    Developer Card

--- src/index.js
+++ src/index.js
@@ -1,17 +1,87 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const skillsData = [
+  {
+    skill: 'HTML+CSS',
+    level: 'advanced',
+    color: 'green',
+  },
+
+  {
+    skill: 'JavaScript',
+    level: 'intermidiate',
+    color: 'yellow',
+  },
+
+  {
+    skill: 'React',
+    level: 'beginner',
+    color: 'red',
+  },
+];
+
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return <img src="/img_1888.jpg" alt="Yuliya" className="avatar" />;
+}
+
+function Intro() {
+  return (
+    <div className="data">
+      <h2>Yuliya</h2>
+      <p>Info about Yuliya</p>
+    </div>
+  );
+}
+
+function SkillList() {
+  const skills = skillsData;
+  return (
+    <ul className="skill-list">
+      {skills.map((skill) => (
+        <Skill skillObj={skill} key={skill.skill} />
+      ))}
+    </ul>
+  );
+}
+
+function Skill({ skillObj }) {
+  const emoji =
+    skillObj.level === 'beginner'
+      ? '👏'
+      : skillObj.level === 'intermidiate'
+      ? '👍'
+      : skillObj.level === 'advanced'
+      ? '❤'
+      : null;
+
+  return (
+    <li className="skill" style={{ backgroundColor: skillObj.color }}>
+      {skillObj.skill + emoji}
+    </li>
+  );
+}
+
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
